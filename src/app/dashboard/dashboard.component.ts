145f6616734e1ac811abcf81f514ce8812f89e7f
kabzangwana <79355407+kabzangwana@users.noreply.github.com>
2023-07-31 09:50:05 +0200

    completed getHero

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class HeroesComponent {
+export class DashboardComponent {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
@@ -18,6 +18,7 @@ export class HeroesComponent {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
+
 }
